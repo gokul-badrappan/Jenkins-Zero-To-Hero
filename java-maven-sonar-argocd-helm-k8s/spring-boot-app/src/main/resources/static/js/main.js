5e7945ca041263bc4c406f7f592efe7c69b0680c
Gokul Badrappan <120174035+gokul-badrappan@users.noreply.github.com>
2025-04-21 00:20:24 +0530

    Update main.js

---
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.btn-primary');
+    ctaButton.addEventListener('click', function() {
+        alert("Let's get started!");
+    });
+});
